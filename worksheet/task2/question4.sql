ef68dfe51fa87f69fb2e566c76d1fefc2dbe08cb
Berk-hub <qcnd0994@leeds.ac.uk>
2026-02-02 11:15:24 +0000

    Fix SQL query to match table and column names

--- worksheet/task2/question4.sql
+++ worksheet/task2/question4.sql
@@ -1,10 +1,11 @@
 -- List all students along with the names of the courses they are enrolled in.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, CourseName
-SELECT s.StudentId, s.FirstName, s.LastName, c.CourseName
-FROM Student s
-LEFT JOIN Enrolment e
-  ON s.StudentId = e.StudentId
-LEFT JOIN Course c
-  ON e.CourseId = c.CourseId;
-
+SELECT
+    s.StudentId,
+    s.FirstName,
+    s.LastName,
+    c.CourseName
+FROM Students s
+LEFT JOIN Enrolments e ON s.StudentId = e.StudentId
+LEFT JOIN Courses c ON e.CourseId = c.CourseId;
