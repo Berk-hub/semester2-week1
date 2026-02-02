d0a564205869983fa79c3eb4e556fc7456bb5707
Berk-hub <qcnd0994@leeds.ac.uk>
2026-02-02 11:18:23 +0000

    Change column names to lowercase in SQL query

--- worksheet/task2/question4.sql
+++ worksheet/task2/question4.sql
@@ -2,10 +2,10 @@
 -- Expected Columns:
 -- StudentId, FirstName, LastName, CourseName
 SELECT
-    s.StudentId,
-    s.FirstName,
-    s.LastName,
-    c.CourseName
-FROM Students s
-LEFT JOIN Enrolments e ON s.StudentId = e.StudentId
-LEFT JOIN Courses c ON e.CourseId = c.CourseId;
+    s.student_id AS StudentId,
+    s.first_name AS FirstName,
+    s.last_name AS LastName,
+    c.course_name AS CourseName
+FROM students s
+LEFT JOIN enrolments e ON s.student_id = e.student_id
+LEFT JOIN courses c ON e.course_id = c.course_id;
