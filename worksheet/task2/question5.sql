b176b3e182a36539c95815de06b0d39ccc302ae6
Berk-hub <qcnd0994@leeds.ac.uk>
2026-02-02 11:02:02 +0000

    Fix SQL query to calculate total credits passed

--- worksheet/task2/question5.sql
+++ worksheet/task2/question5.sql
@@ -2,11 +2,10 @@
 -- Expected Columns:
 -- StudentId, FirstName, LastName, TotalCreditsPassed
 SELECT s.StudentId, s.FirstName, s.LastName,
-       COALESCE(SUM(c.Credits), 0) AS TotalCreditsPassed
+       COALESCE(SUM(CASE WHEN e.Grade >= 40 THEN c.Credits ELSE 0 END), 0) AS TotalCreditsPassed
 FROM Student s
-LEFT JOIN Enrollment e
+LEFT JOIN Enrolment e
   ON s.StudentId = e.StudentId
- AND e.Grade >= 40
 LEFT JOIN Course c
   ON e.CourseId = c.CourseId
 GROUP BY s.StudentId, s.FirstName, s.LastName;
