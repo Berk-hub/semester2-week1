934e848cc670bb9ba190afe074024c8401cf7403
Berk-hub <qcnd0994@leeds.ac.uk>
2026-02-02 10:56:36 +0000

    Fix SQL query for student enrollment in 2022

--- worksheet/task2/question1.sql
+++ worksheet/task2/question1.sql
@@ -1,6 +1,6 @@
 -- List all students who enrolled in the year 2022.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, EnrolmentYear
-SELECT StudentId, FirstName, LastName, EnrolmentYear
-FROM Students
-WHERE EnrolmentYear = 2022;
+SELECT StudentId, FirstName, LastName, EnrollmentYear AS EnrolmentYear
+FROM Student
+WHERE EnrollmentYear = 2022;
